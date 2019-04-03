4063b322a226f4a215bc225263628777a82aa8d4
Mikhail Gorodetsky <topin89@mail.ru>
2019-04-03 12:34:35 +0300

    Correct use of openmp

--- raytracing.c
+++ raytracing.c
@@ -481,42 +481,47 @@ void raytracing(uint8_t *pixels, color background_color,
                 light_node lights, const viewpoint *view,
                 int width, int height)
 {
-    point3 u, v, w, d;
-    color object_color = { 0.0, 0.0, 0.0 };
-
-    /* calculate u, v, w */
-    calculateBasisVectors(u, v, w, view);
-
-    idx_stack stk;
+    
 
     int factor = sqrt(SAMPLES);
     
-    #pragma omp parallel for
-    for (int j = 0; j < height; j++) {
-        for (int i = 0; i < width; i++) {
-            double r = 0, g = 0, b = 0;
-            /* MSAA */
-            for (int s = 0; s < SAMPLES; s++) {
-                idx_stack_init(&stk);
-                rayConstruction(d, u, v, w,
-                                i * factor + s / factor,
-                                j * factor + s % factor,
-                                view,
-                                width * factor, height * factor);
-                if (ray_color(view->vrp, 0.0, d, &stk, rectangulars, spheres,
-                              lights, object_color,
-                              MAX_REFLECTION_BOUNCES)) {
-                    r += object_color[0];
-                    g += object_color[1];
-                    b += object_color[2];
-                } else {
-                    r += background_color[0];
-                    g += background_color[1];
-                    b += background_color[2];
+    #pragma omp parallel
+    {
+        point3 u, v, w, d;
+        color object_color = { 0.0, 0.0, 0.0 };
+
+        /* calculate u, v, w */
+        calculateBasisVectors(u, v, w, view);
+
+        idx_stack stk;
+        
+        #pragma omp for
+        for (int j = 0; j < height; j++) {
+            for (int i = 0; i < width; i++) {
+                double r = 0, g = 0, b = 0;
+                /* MSAA */
+                for (int s = 0; s < SAMPLES; s++) {
+                    idx_stack_init(&stk);
+                    rayConstruction(d, u, v, w,
+                                    i * factor + s / factor,
+                                    j * factor + s % factor,
+                                    view,
+                                    width * factor, height * factor);
+                    if (ray_color(view->vrp, 0.0, d, &stk, rectangulars, spheres,
+                                  lights, object_color,
+                                  MAX_REFLECTION_BOUNCES)) {
+                        r += object_color[0];
+                        g += object_color[1];
+                        b += object_color[2];
+                    } else {
+                        r += background_color[0];
+                        g += background_color[1];
+                        b += background_color[2];
+                    }
+                    pixels[((i + (j * width)) * 3) + 0] = r * 255 / SAMPLES;
+                    pixels[((i + (j * width)) * 3) + 1] = g * 255 / SAMPLES;
+                    pixels[((i + (j * width)) * 3) + 2] = b * 255 / SAMPLES;
                 }
-                pixels[((i + (j * width)) * 3) + 0] = r * 255 / SAMPLES;
-                pixels[((i + (j * width)) * 3) + 1] = g * 255 / SAMPLES;
-                pixels[((i + (j * width)) * 3) + 2] = b * 255 / SAMPLES;
             }
         }
     }
